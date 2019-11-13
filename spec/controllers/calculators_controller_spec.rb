003b7bb6cfc1897ce0d0e4da769c54faf22c560b
LindaKadz <achielinda44@gmail.com>
2019-11-12 17:47:46 -0800

    add test

--- spec/controllers/calculators_controller_spec.rb
+++ spec/controllers/calculators_controller_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe CalculatorsController, type: :controller do
-
+  it 'should create a record' do
+   calculator = Calculator.create({principle: 5000, year: 3, rate: 2.5})
+   calculator.valid?
+   expect(calculator).to include "created"
+  end
 end
