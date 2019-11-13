92245ccb859138a168d5531a95dafc70cd1311af
LindaKadz <achielinda44@gmail.com>
2019-11-12 17:44:02 -0800

    test the model

---
+++ spec/factories/calculators.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :calculator do
+    principle { 5000 }
+    year  { 5 }
+    rate { 2.5 }
+  end
+end
--- spec/models/calculator_spec.rb
+++ spec/models/calculator_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Calculator, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'should validate presence of principle' do
+   calculator = Calculator.new({year: 3, rate: 2.5})
+   calculator.valid?
+   expect(calculator.errors.messages[:principle]).to include "can't be blank"
+  end
+
+  it { should validate_presence_of(:year)  }
+  it { should validate_presence_of(:principle)  }
+  it { should validate_presence_of(:rate)  }
 end
