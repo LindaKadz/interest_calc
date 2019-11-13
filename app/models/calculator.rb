55d70c8fe34d69b34bb865963b45f20cb7cea01f
LindaKadz <achielinda44@gmail.com>
2019-11-12 16:53:33 -0800

    add numericality validation

--- app/models/calculator.rb
+++ app/models/calculator.rb
@@ -1,9 +1,9 @@
 class Calculator < ApplicationRecord
   before_save :get_total
 
-  validates :principle, presence: true
-  validates :year, presence: true
-  validates :rate, presence: true
+  validates :principle, presence: true, numericality: true
+  validates :year, presence: true, numericality: true
+  validates :rate, presence: true, numericality: true
 
   def get_total
     self.total = principle * ( 1 +((rate/100) * year))
