4abd055ad716b47fefb93f30d6d8fc055161115f
LindaKadz <achielinda44@gmail.com>
2019-11-12 16:49:52 -0800

    added presence validations

--- app/models/calculator.rb
+++ app/models/calculator.rb
@@ -1,5 +1,10 @@
 class Calculator < ApplicationRecord
   before_save :get_total
+
+  validates :principle, presence: true
+  validates :year, presence: true
+  validates :rate, presence: true
+
   def get_total
     self.total = principle * ( 1 +((rate/100) * year))
   end
